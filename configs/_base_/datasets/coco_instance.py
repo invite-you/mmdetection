edac36737429edf30a7fc73443833301e346b2c5
invite-you <50567167+invite-you@users.noreply.github.com>
2020-10-25 19:01:29 +0900

    Update coco_instance.py

--- configs/_base_/datasets/coco_instance.py
+++ configs/_base_/datasets/coco_instance.py
@@ -1,28 +1,38 @@
-dataset_type = 'CocoDataset'
-data_root = 'data/coco/'
+dataset_type = 'CocoDatasetCar'
+data_root = '/content/gdrive/My Drive/Arirang/data/train/custom_coco_all_car/'
+imgae_root = '/content/gdrive/My Drive/Arirang/data/train/coco_all/'
 img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+    mean=[54.06, 53.295, 50.235], std=[36.72, 35.955, 33.915], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
+    dict(type='RandomCrop', crop_size=(640, 640)),    
     dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='PhotoMetricDistortion',
+         brightness_delta=32,
+         contrast_range=(0.1, 1.1),
+         saturation_range=(0.1, 1.1),
+         hue_delta=18),
+    dict(type='CutOut', n_holes=(0, 1),
+         cutout_shape=[(250, 100), (250, 250), (100, 250), (100, 100)]),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
+    #dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
+    #dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
+        img_scale=(1024, 1024),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
             dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
+            #dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
         ])
@@ -33,16 +43,16 @@ data = dict(
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_train2017.json',
-        img_prefix=data_root + 'train2017/',
+        img_prefix=imgae_root + 'train2017/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        img_prefix=imgae_root + 'val2017/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file= '/content/gdrive/My Drive/Arirang/data/test/instances_test2017.json',
+        img_prefix= '/content/gdrive/My Drive/Arirang/data/test/images/',
         pipeline=test_pipeline))
 evaluation = dict(metric=['bbox', 'segm'])
