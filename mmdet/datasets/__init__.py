b07444dfb1a4c8f3b4d7bd6487364772bea9cdbe
invite-you <50567167+invite-you@users.noreply.github.com>
2020-10-25 18:56:58 +0900

    Update __init__.py

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -14,7 +14,7 @@ from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
 
 __all__ = [
-    'CustomDataset', 'XMLDataset', 'CocoDataset', 'DeepFashionDataset',
+    'CustomDataset', 'XMLDataset', 'CocoDataset', 'CocoDatasetCar', 'DeepFashionDataset',
     'VOCDataset', 'CityscapesDataset', 'LVISDataset', 'LVISV05Dataset',
     'LVISV1Dataset', 'GroupSampler', 'DistributedGroupSampler',
     'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
