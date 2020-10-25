4cd67e875cae4aab5e463b7b3b9f469b03cc8a97
invite-you <50567167+invite-you@users.noreply.github.com>
2020-10-25 18:56:31 +0900

    Update __init__.py

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -1,6 +1,7 @@
 from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
 from .cityscapes import CityscapesDataset
 from .coco import CocoDataset
+from .coco_car import CocoDatasetCar
 from .custom import CustomDataset
 from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
                                RepeatDataset)
